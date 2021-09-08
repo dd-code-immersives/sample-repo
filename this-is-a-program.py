2d0d34082ea9341021c17a7a19779701e72dec54
James C H. Weaver <jamesweaver@jamess-mbp.home>
2021-09-08 15:21:38 -0400

    minor change added

--- this-is-a-program.py
+++ this-is-a-program.py
@@ -2,7 +2,7 @@ print("this is a test")
 print("on dev")
 print("dominic doyle")
 print("im another developer somewhere that made changes")
-print('James')
+print('James Weaver')
 print("tony did the thing")
 print('Trey')
 
